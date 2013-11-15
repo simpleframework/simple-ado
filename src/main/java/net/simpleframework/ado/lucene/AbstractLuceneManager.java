950260ff8023f337727b99680c1beb5f07ea03e9
simpleframework <cknet@126.com>
2013-11-15 12:10:01 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/ado/lucene/AbstractLuceneManager.java
+++ src/main/java/net/simpleframework/ado/lucene/AbstractLuceneManager.java
@@ -38,8 +38,7 @@ import org.apache.lucene.util.Version;
 /**
  * Licensed under the Apache License, Version 2.0
  * 
- * @author 陈侃(cknet@126.com, 13910090885)
- *         https://github.com/simpleframework
+ * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public abstract class AbstractLuceneManager extends AbstractADOManager implements ILuceneManager {
