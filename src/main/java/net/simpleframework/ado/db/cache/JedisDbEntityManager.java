a363856ab7e6792666484aa769a904476e803a83
simpleframework <cknet@126.com>
2014-06-22 02:28:13 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/ado/db/cache/JedisDbEntityManager.java
+++ src/main/java/net/simpleframework/ado/db/cache/JedisDbEntityManager.java
@@ -3,6 +3,7 @@ package net.simpleframework.ado.db.cache;
 import net.simpleframework.ado.db.DbEntityTable;
 import net.simpleframework.common.IoUtils;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 /**
  * Licensed under the Apache License, Version 2.0
@@ -19,14 +20,14 @@ public class JedisDbEntityManager<T> extends AbstractCacheDbEntityManager<T> {
 		super(null);
 	}
 
-	private Jedis jedis;
+	private JedisPool pool;
 
-	public void setJedis(final Jedis jedis) {
-		this.jedis = jedis;
+	public void setJedisPool(final JedisPool pool) {
+		this.pool = pool;
 	}
 
-	public Jedis getJedis() {
-		return jedis;
+	private Jedis getJedis() {
+		return pool.getResource();
 	}
 
 	@Override
@@ -34,7 +35,7 @@ public class JedisDbEntityManager<T> extends AbstractCacheDbEntityManager<T> {
 		try {
 			final String id = idCache.get(key);
 			if (id != null) {
-				return IoUtils.deserialize(jedis.get(id.getBytes()));
+				return IoUtils.deserialize(getJedis().get(id.getBytes()));
 			}
 		} catch (final Exception e) {
 			log.warn(e);
@@ -48,7 +49,7 @@ public class JedisDbEntityManager<T> extends AbstractCacheDbEntityManager<T> {
 			final String id = getId(val);
 			if (id != null) {
 				idCache.put(key, id);
-				jedis.set(id.getBytes(), IoUtils.serialize(val));
+				getJedis().set(id.getBytes(), IoUtils.serialize(val));
 			}
 		} catch (final Exception e) {
 			log.warn(e);
@@ -59,7 +60,7 @@ public class JedisDbEntityManager<T> extends AbstractCacheDbEntityManager<T> {
 	public void removeCache(final String key) {
 		final String id = idCache.remove(key);
 		if (id != null) {
-			jedis.del(id.getBytes());
+			getJedis().del(id.getBytes());
 		}
 	}
 
@@ -67,7 +68,7 @@ public class JedisDbEntityManager<T> extends AbstractCacheDbEntityManager<T> {
 	public void removeVal(final Object val) {
 		final String id = getId(val);
 		if (id != null) {
-			jedis.del(id.getBytes());
+			getJedis().del(id.getBytes());
 		}
 	}
 
